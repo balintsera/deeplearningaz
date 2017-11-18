af869b9b1842979a4ec470c823c2ea2ea5e5bdd5
Séra Bálint <sera.balint@e-vista.hu>
2017-11-18 18:08:24 +0100

    self organizing maps

--- rnn.py
+++ rnn.py
@@ -28,8 +28,8 @@ X_train, y_train = np.array(X_train), np.array(y_train)
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
 
-#%%
-def rnn():
+#%% 0.0038
+def rnn(epochs=2, units=50):
     """ Build network here
     """
     from keras.models import Sequential
@@ -42,10 +42,12 @@ def rnn():
 
     # Adding LSTM layers
     layers = [
-        {'layer': LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)), 'dropout': Dropout(0.2)},
-        {'layer': LSTM(units=50, return_sequences=True), 'dropout': Dropout(0.2)},
-        {'layer': LSTM(units=50, return_sequences=True), 'dropout': Dropout(0.2)},
-        {'layer': LSTM(units=50), 'dropout': Dropout(0.2)},
+        {'layer': LSTM(units=units, return_sequences=True, input_shape=(X_train.shape[1], 1)), 'dropout': Dropout(0.2)},
+        {'layer': LSTM(units=units, return_sequences=True), 'dropout': Dropout(0.2)},
+        {'layer': LSTM(units=units, return_sequences=True), 'dropout': Dropout(0.2)},
+        {'layer': LSTM(units=units, return_sequences=True), 'dropout': Dropout(0.2)},
+        {'layer': LSTM(units=units, return_sequences=True), 'dropout': Dropout(0.2)},
+        {'layer': LSTM(units=units), 'dropout': Dropout(0.2)},
         {'layer': Dense(units=1)}
         ]
     for layer in layers:
@@ -54,12 +56,12 @@ def rnn():
             regressor.add(layer['dropout'])
 
     regressor.compile(optimizer='adam', loss='mean_squared_error')
-    regressor.fit(X_train, y_train, epochs=2, batch_size=32)
+    regressor.fit(X_train, y_train, epochs=epochs, batch_size=32)
     return regressor
 
-regressor = rnn()
+regressor = rnn(epochs=8, units=200)
 #%% Part 3 - Making the prediction
-dataset_test = pd.read_csv('/work/rnn/dataset/Google_Stock_Price_Test.csv')
+dataset_test = pd.read_csv('/work/rnn-dataset/Google_Stock_Price_Test.csv')
 dataset_test.iloc[:, 1:2].shape
 real_stock_price = dataset_test.iloc[:, 1:2]
 real_stock_price
@@ -71,7 +73,7 @@ inputs = inputs.reshape(-1, 1)
 inputs = sc.transform(inputs)
 X_test = []
 for i in range(60, 80):
-    X_test.append(X_test)
+    X_test.append(inputs[i-60:i, 0])
 X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 predicted_stock_price = regressor.predict(X_test)
@@ -80,8 +82,8 @@ predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 #%% Visualizing the results
 plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
 plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted')
-plt.tilte('Google Stock Price Prediction')
+plt.title('Google Stock Price Prediction')
 plt.xlabel('Time')
-plt.label('Google Stock Price')
+plt.ylabel('Google Stock Price')
 plt.legend()
 plt.show()
